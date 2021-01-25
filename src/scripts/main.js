2263ad8909221647ee0176a60f973ccb98eddd72
bpotts11 <brittneympotts@gmail.com>
2021-01-24 23:22:40 -0600

    created module for all seed types. exported function with information about height and output of each seed. tested by importing all seeds to main.js and console logging to make sure all functions worked

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,3 +3,8 @@ import { createPlan } from "./plan.js"
 
 const yearlyPlan = createPlan()
 console.log(yearlyPlan)
+
+import { createWheat } from "./seeds/wheat.js"
+
+const wheatSeed = createWheat()
+console.log(wheatSeed)
