ae1657a00bea07716f0d1a2468dd8a602d7d8242
bpotts11 <brittneympotts@gmail.com>
2021-02-28 22:49:44 -0600

    added comments

--- src/scripts/harvester.js
+++ src/scripts/harvester.js
@@ -2,23 +2,25 @@
 let harvestArray = []
 
 // harvestPlants function must accept the plants array as input.
-// The function will return an array of seed objects.
-// Iterate the array of growing plants. On each plant, get the value of the output property.
-// Add that many of the plant objects to the array that the function returns. 
 export const harvestPlants = (plantArray) => {
     // debugger
+    // Iterate the array of growing plants. On each plant, get the value of the output property.
     for (const plant of plantArray) {
         let output = plant.output
+        // Half of your corn will be sold to cattle ranchers, so only half of the output of each corn plant will be added to the array that this function returns.
         if (plant.type === "Corn") {
             let cornOutput = output / 2
+            // Add that many of the plant objects to the array that the function returns. 
             for (let i = 0; i < cornOutput; i++) {
                 harvestArray.push(plant)
             }
         } else {
+            // Add that many of the plant objects to the array that the function returns. 
             for (let i = 0; i < output; i++) {
                 harvestArray.push(plant)
             }
         }
     }
+    // The function will return an array of seed objects.
     return harvestArray
 }
